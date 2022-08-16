0236006892d88bdd3b8252ce98636a9d157aa125
Alper Guven <10487846+alper-guven@users.noreply.github.com>
2022-08-16 20:48:19 +0300

    0.0.4

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createRedisKey = void 0;
 console.log('Create Redis Key');
+var createRedisKey = function () { return 'Create Redis Key'; };
+exports.createRedisKey = createRedisKey;
